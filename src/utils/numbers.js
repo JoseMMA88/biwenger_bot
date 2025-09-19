1b9b2586af7719f5f682700692e4ad806bee6674
Jose Manuel Malagón Alba <contact@josemalagon.es>
2025-09-19 12:18:14 +0200

    Adding logger

--- src/utils/numbers.js
+++ src/utils/numbers.js
@@ -1,4 +1,4 @@
-const pickNumber = (...vals) => {
+const PickNumber = (...vals) => {
   for (const v of vals) {
     const n = Number(v);
     if (Number.isFinite(n)) return n;
@@ -6,6 +6,6 @@ const pickNumber = (...vals) => {
   return undefined;
 };
 
-const euro = (n) => new Intl.NumberFormat('es-ES').format(n) + ' €';
+const Euro = (n) => new Intl.NumberFormat('es-ES').format(n) + ' €';
 
-module.exports = { pickNumber, euro };
+module.exports = { PickNumber, Euro };
